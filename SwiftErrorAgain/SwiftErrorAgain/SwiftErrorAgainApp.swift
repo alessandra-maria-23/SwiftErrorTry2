f06a317ddf97e2e44f511f9d6d179b1e24464380
Alessandra Iorga <a.iorga@student.fontys.nl>
2023-11-15 12:39:42 +0100

    create project

---
+++ SwiftErrorAgain/SwiftErrorAgain/SwiftErrorAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftErrorAgainApp.swift
+//  SwiftErrorAgain
+//
+//  Created by Alessandra Iorga on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftErrorAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
